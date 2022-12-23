dd061faee58b9a7d8195c65cd1bb3aa635183edb
hoavic <oceo.info@gmail.com>
2022-12-23 17:32:09 +0700

    23/12/2022: ads icon, form detail

--- footer.php
+++ footer.php
@@ -11,6 +11,22 @@
 
 ?>
 	</div>
+	<div class="promo-icon">
+		<div class="ads-grid">
+			<div class="ads-item">
+				<img src="/wp-content/themes/khahan/img/vimientay.svg" alt="Lạp xưởng chuẩn vị miền tây" />
+				<span class="ads-text">CHUẨN VỊ MIỀN TÂY</span>
+			</div>
+			<div class="ads-item">
+				<img src="/wp-content/themes/khahan/img/khongbaoquan.svg" alt="không chất bảo quản" />
+				<span class="ads-text">KHÔNG HÓA CHẤT</span>
+			</div>
+			<div class="ads-item">
+				<img src="/wp-content/themes/khahan/img/hangmoi.svg" alt="100% hàng mới" />
+				<span class="ads-text">HÀNG MỚI<br/>ĐẶT TRƯỚC</span>
+			</div>
+		</div>
+	</div>
 	<footer id="colophon" class="site-footer">
 		
 		<p class="title-up">ĐẶC SẢN LẠP XƯỞNG TIỀN GIANG</p>
